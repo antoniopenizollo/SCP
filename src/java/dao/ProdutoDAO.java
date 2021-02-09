d5f366b112f58606f0d02176579e77bf31665a24
Antônio Penizollo <antoniopenizollo@gmail.com>
2021-02-09 20:22:42 -0300

    implementando classe ProdutoDAO

--- src/java/dao/ProdutoDAO.java
+++ src/java/dao/ProdutoDAO.java
@@ -10,5 +10,39 @@ package dao;
  * @author anton
  */
 public class ProdutoDAO {
+    private Integer codigo;
+    private String nome;
+    private Float preco;
+
+    public ProdutoDAO(Integer codigo, String nome, Float preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+    
     
 }
