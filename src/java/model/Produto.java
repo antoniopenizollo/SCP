fe9107356abd7073f1f179bc57d86af6d79e38a3
Antônio Penizollo <antoniopenizollo@gmail.com>
2021-02-09 20:11:33 -0300

    criando classe Produto

---
+++ src/java/model/Produto.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author anton
+ */
+public class Produto {
+    private Integer codigo;
+    private String nome;
+    private Float preco;
+
+    public Produto(Integer codigo, String nome, Float preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+    
+    
+}
